510fd1fb18c24366d385daa26f5f3b595352ef1e
Thomas Jalabert <thom.jalabert@gmail.com>
2019-01-07 00:37:32 +0100

    add build launcher

--- build.jl
+++ build.jl
@@ -1,6 +1,13 @@
-using PackageCompiler
+#using PackageCompiler
 
 #build_executable("TravelingSalesman.jl")
 
-dd = normpath(Base.find_package("PackageCompiler"), "..", "..")
-print(dd)
+#dd = normpath(Base.find_package("PackageCompiler"), "..", "..")
+#print(dd)
+
+#build_executable("GenerateFormula3.jl")
+
+path = normpath(Base.find_package("PackageCompiler"), "..", "..")
+command = "julia " * path * "juliac.jl -vRe ./GenerateFormula3.jl"
+#print(command)
+run(`$command`)
